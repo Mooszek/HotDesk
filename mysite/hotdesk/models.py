c38ecf457798dfaf94ef58d931f2b2dbc22bd2f7
Mooszek <mooszek@gmail.com>
2022-12-21 21:23:11 +0100

    Migration fix + first view

--- mysite/hotdesk/models.py
+++ mysite/hotdesk/models.py
@@ -2,18 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-    room_id = models.IntegerField(unique=True)
+    #room_id = models.IntegerField(unique=True)
     room_name = models.CharField(unique=True, max_length=20)
     desk_capacity = models.IntegerField()
 
+    def __str__(self) -> str:
+        return self.room_name
+
 class Desk(models.Model):
-    desk_id = models.IntegerField(unique=True)
     desk_label = models.CharField(unique=True, max_length=20)
-    room_id = models.ForeignKey(Room, on_delete=models.SET_NULL)
+    room = models.ForeignKey(Room, default=None, blank= True, null=True, on_delete=models.SET_NULL)
     monitors_count = models.IntegerField(default=0)
     reservation_flag = models.BooleanField(default=False)
 
+    def __str__(self) -> str:
+        return self.desk_label
+
 class Reservation(models.Model):
-    reservation_id = models.IntegerField(unique=True)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"Reservation ID: {self.id}"
--- mysite/hotdesk/views.py
+++ mysite/hotdesk/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hotdesk.models import Reservation
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("This will be hotdesking app")
+
+def reservation(request, reservation_id):
+    reservation_object = Reservation.objects.get(pk=reservation_id)
+    start_date = reservation_object.start_date
+    end_date = reservation_object.end_date
+    response = f"Reservation ID: {reservation_id} Start Date: {start_date} End Date: {end_date}"
+    return HttpResponse(response)
+
